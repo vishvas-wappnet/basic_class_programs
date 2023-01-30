0b955a56377e5a8e8aaba8fe4e8f45f252a3fe72
MrRd016 <rronak016@gmail.com>
2023-01-30 15:43:00 +0530

    object class file

---
+++ object_class.php
@@ -0,0 +1,13 @@
+<?php
+    class bank
+    {
+        public $account_name="Ronak";
+        public $account_number=3002;
+        public function show_data(){
+            echo"welcome : ".$this->account_name."your account number is : ".$this->account_number;
+        }
+    }
+    $user1=new bank();
+    $user1->show_data();
+
+?>
